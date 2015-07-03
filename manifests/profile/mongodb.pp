ecd42bbcfe2fe542f1bda12cd74c91822787a12e
Damien Churchill <damien.churchill@cloudbuy.com>
2015-07-03 08:41:35 +0100

    fix setting bind address for HA controllers

--- manifests/profile/mongodb.pp
+++ manifests/profile/mongodb.pp
@@ -1,13 +1,14 @@
 # The profile to install an OpenStack specific MongoDB server
 class openstack::profile::mongodb {
   $management_network = $::openstack::config::network_management
+  $management_address = ip_for_network($management_network)
 
   class { '::mongodb::globals':
     manage_package_repo => true,
   }
 
   class { '::mongodb::server':
-    bind_ip => ['127.0.0.1', $::openstack::config::controller_address_management],
+    bind_ip => ['127.0.0.1', $management_address],
   }
 
   class { '::mongodb::client': }
