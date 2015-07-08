ddd6b20de9820d040a7ef8b055c7f6ca66cdc038
Damien Churchill <damoxc@gmail.com>
2015-07-09 00:09:10 +0100

    listen on the IP address for the management network, not the specified controller address

--- manifests/profile/memcache.pp
+++ manifests/profile/memcache.pp
@@ -1,7 +1,11 @@
 # The profile to install a local instance of memcache
 class openstack::profile::memcache {
+
+  $management_network = $::openstack::config::network_management
+  $management_address = ip_for_network($management_network)
+
   class { 'memcached':
-    listen_ip => $::openstack::config::controller_address_management, #'127.0.0.1',
+    listen_ip => $management_address,
     tcp_port  => '11211',
     udp_port  => '11211',
   }
