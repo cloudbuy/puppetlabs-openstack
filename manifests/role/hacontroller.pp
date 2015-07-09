fec66d1e34ca9b7423db0c7e06ea74e8f4f85c8d
Damien Churchill <damoxc@gmail.com>
2015-07-09 09:13:37 +0100

    re-enable all the profiles bar the swift proxy, lets see what breaks

--- manifests/role/hacontroller.pp
+++ manifests/role/hacontroller.pp
@@ -5,14 +5,13 @@ class openstack::role::hacontroller inherits ::openstack::role {
   class { '::openstack::profile::galera': } ->
   class { '::openstack::profile::mongodb': }
 
-  #class { '::openstack::profile::keystone': } ->
-  #class { '::openstack::profile::swift::proxy': } ->
-  #class { '::openstack::profile::ceilometer::api': } ->
-  #class { '::openstack::profile::glance::auth': } ->
-  #class { '::openstack::profile::cinder::api': } ->
-  #class { '::openstack::profile::nova::api': } ->
-  #class { '::openstack::profile::neutron::server': } ->
-  #class { '::openstack::profile::heat::api': } ->
-  #class { '::openstack::profile::horizon': }
-  #class { '::openstack::profile::auth_file': }
+  class { '::openstack::profile::keystone': } ->
+  class { '::openstack::profile::ceilometer::api': } ->
+  class { '::openstack::profile::glance::auth': } ->
+  class { '::openstack::profile::cinder::api': } ->
+  class { '::openstack::profile::nova::api': } ->
+  class { '::openstack::profile::neutron::server': } ->
+  class { '::openstack::profile::heat::api': } ->
+  class { '::openstack::profile::horizon': }
+  class { '::openstack::profile::auth_file': }
 }
