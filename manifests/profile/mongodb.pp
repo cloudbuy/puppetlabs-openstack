77d27de4a2484d91937dfe108e6f19a54af5a384
Damien Churchill <damoxc@gmail.com>
2015-07-09 14:08:32 +0100

    begin the groundwork for being able to configure a mongoDB replicaset

--- manifests/profile/mongodb.pp
+++ manifests/profile/mongodb.pp
@@ -3,6 +3,14 @@ class openstack::profile::mongodb {
   $management_network = $::openstack::config::network_management
   $management_address = ip_for_network($management_network)
 
+  $controller = $::openstack::controllers[$::hostname]
+  if ($controller['primary']) {
+    $is_primary = true
+  } else {
+    $is_primary = false
+  }
+  notice("Primary: $is_primary")
+
   class { '::mongodb::globals':
     manage_package_repo => true,
   }
