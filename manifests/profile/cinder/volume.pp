4826eebd6985e20b4fc260b179dc5e71af9b0294
Damien Churchill <damoxc@gmail.com>
2016-01-08 09:52:05 +0000

    hard-code our cinder rbd configuratinon as a test

--- manifests/profile/cinder/volume.pp
+++ manifests/profile/cinder/volume.pp
@@ -3,22 +3,41 @@ class openstack::profile::cinder::volume {
   $management_network = $::openstack::config::network_management
   $management_address = ip_for_network($management_network)
 
-  openstack::resources::firewall { 'ISCSI API': port => '3260', }
-
   include ::openstack::common::cinder
-
-  class { '::cinder::setup_test_volume':
-    volume_name => 'cinder-volumes',
-    size        => $::openstack::config::cinder_volume_size
-  } ->
 
   class { '::cinder::volume':
     package_ensure => present,
     enabled        => true,
   }
 
-  class { '::cinder::volume::iscsi':
-    iscsi_ip_address => $management_address,
-    volume_group     => 'cinder-volumes',
+  cinder::backend::rbd {'rbd-ssd':
+    rbd_pool                         => 'cinder-volumes-ssd',
+    rbd_user                         => 'cinder',
+    rbd_secret_uuid                  => '3619fcb5-e5eb-4435-93ff-d2e4ccfdd95a',
+    rbd_ceph_conf                    => '/etc/ceph/ceph.conf',
+    rbd_flatten_volume_from_snapshot => false,
+    rbd_max_clone_depth              => '5',
+  }
+
+  cinder::backend::rbd {'rbd-scsi':
+    rbd_pool                         => 'cinder-volumes-scsi',
+    rbd_user                         => 'cinder',
+    rbd_secret_uuid                  => '3619fcb5-e5eb-4435-93ff-d2e4ccfdd95a',
+    rbd_ceph_conf                    => '/etc/ceph/ceph.conf',
+    rbd_flatten_volume_from_snapshot => false,
+    rbd_max_clone_depth              => '5',
+  }
+
+  cinder::backend::rbd {'rbd-sata':
+    rbd_pool                         => 'cinder-volumes-sata',
+    rbd_user                         => 'cinder',
+    rbd_secret_uuid                  => '3619fcb5-e5eb-4435-93ff-d2e4ccfdd95a',
+    rbd_ceph_conf                    => '/etc/ceph/ceph.conf',
+    rbd_flatten_volume_from_snapshot => false,
+    rbd_max_clone_depth              => '5',
+  }
+
+  class { '::cinder::backends':
+    enabled_backends => ['rbd-ssd', 'rbd-scsi', 'rbd-sata']
   }
 }
