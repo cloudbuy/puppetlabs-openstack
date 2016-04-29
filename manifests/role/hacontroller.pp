d2f5ecd7a78952c2b208139ea55b95bc0ac8d58c
Damien Churchill <damoxc@gmail.com>
2016-04-29 15:22:15 +0100

    tweak the order of imported classes

--- manifests/role/hacontroller.pp
+++ manifests/role/hacontroller.pp
@@ -5,9 +5,8 @@ class openstack::role::hacontroller inherits ::openstack::role {
   class { '::openstack::profile::galera': } ->
   class { '::openstack::profile::mongodb': }
 
-  class { '::openstack::profile::haproxy::controller': }
-  class { '::openstack::profile::keepalived::controller': }
-
+  class { '::openstack::profile::keepalived::controller': }->
+  class { '::openstack::profile::haproxy::controller': }->
   class { '::openstack::profile::keystone': } ->
   class { '::openstack::profile::ceilometer::api': } ->
   class { '::openstack::profile::glance::auth': } ->
