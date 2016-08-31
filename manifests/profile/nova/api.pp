2a21b4bf832ec3f14b8582bc7f4cf8bf749bc33a
Damien Churchill <damoxc@gmail.com>
2016-08-31 12:42:30 +0100

    need to use nova::db::mysql_api directly due to the difference in naming

--- manifests/profile/nova/api.pp
+++ manifests/profile/nova/api.pp
@@ -4,9 +4,12 @@ class openstack::profile::nova::api {
   $controller_management_address = $::openstack::config::controller_address_management
 
   openstack::resources::database { 'nova': }
-  openstack::resources::database { 'nova_api':
-    user     => $::openstack::config::mysql_user_nova,
-    password => $::openstack::config::mysql_pass_nova,
+  class { "::nova::db::mysql_api":
+    user          => $::openstack::config::mysql_user_nova,
+    password      => $::openstack::config::mysql_pass_nova,
+    dbname        => 'nova_api',
+    allowed_hosts => $::openstack::config::mysql_allowed_hosts,
+    require       => Anchor['database-service'],
   }
   openstack::resources::firewall { 'Nova API': port => '8774', }
   openstack::resources::firewall { 'Nova Metadata': port => '8775', }
