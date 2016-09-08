cadc9e3183c2825e0e81a10c7d55afbc82f0382a
Damien Churchill <damien.churchill@cloudbuy.com>
2016-09-08 18:18:08 +0100

    revert neutron back to running outside apache

--- manifests/profile/neutron/server.pp
+++ manifests/profile/neutron/server.pp
@@ -24,43 +24,6 @@ class openstack::profile::neutron::server {
     default => 'http'
   }
 
-  if ($::openstack::config::ssl) {
-    File['/etc/neutron/ssl/key.pem']->
-    ::Openstacklib::Wsgi::Apache['neutron_wsgi']
-    $ssl_cert_file = '/etc/neutron/ssl/cert.pem'
-    $ssl_key_file = '/etc/neutron/ssl/key.pem'
-  } else {
-    $ssl_cert_file = undef
-    $ssl_key_file = undef
-  }
-
-  file { '/usr/lib/cgi-bin/neutron':
-    ensure  => directory,
-    owner   => 'neutron',
-    group   => 'neutron',
-    mode    => '0755',
-    require => Package['httpd'],
-  }->
-  ::openstacklib::wsgi::apache { 'neutron_wsgi':
-    bind_host           => $::openstack::profile::base::management_address,
-    bind_port           => 9696,
-    group               => 'neutron',
-    path                => '/',
-    priority            => '10',
-    servername          => $::openstack::config::controller_address_api,
-    ssl                 => $::openstack::config::ssl,
-    ssl_cert            => $ssl_cert_file,
-    ssl_key             => $ssl_key_file,
-    threads             => $::processorcount,
-    user                => 'neutron',
-    workers             => 1,
-    wsgi_daemon_process => 'neutron-server',
-    wsgi_process_group  => 'neutron-server',
-    wsgi_script_dir     => '/usr/lib/cgi-bin/neutron',
-    wsgi_script_file    => 'neutron-server',
-    wsgi_script_source  => '/usr/bin/neutron-wsgi'
-  }
-
   anchor { 'neutron_common_first': } ->
   class { '::neutron::server::notifications':
     auth_url    => "${scheme}://${controller_management_address}:35357/",
