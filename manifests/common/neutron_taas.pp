6ffa183cc0e8abb2e35c08012e57885454b12e6f
Damien Churchill <damoxc@gmail.com>
2017-05-10 09:46:27 +0100

    greatly improve the taas agent configuration/installation
    This adds the vitally important interface_driver setting to the taas.ini file.

--- manifests/common/neutron_taas.pp
+++ manifests/common/neutron_taas.pp
@@ -1,28 +1,41 @@
 class openstack::common::neutron_taas {
+
+  $package_ensure        = present
+  $enabled               = true
+  $manage_service        = true
+  $taas_driver           = 'neutron_taas.services.taas.drivers.linux.ovs_taas.OvsTaasDriver'
+  $taas_enabled          = true
+  $taas_vlan_range_start = 3000
+  $taas_vlan_range_end   = 3500
+  $interface_driver      = 'neutron.agent.linux.interface.OVSInterfaceDriver'
+  $purge_config          = false
+
   # FIXME: as and when a neutron-taas is added to the neutron module, use that. For now we
   # FIXME: implement it ourselves in this module. 
   package { 'neutron-taas-openvswitch-agent':
-    ensure => installed,
+    ensure => present,
+    'tag'  => ['openstack', 'neutron-package'],
   }->
   file { '/usr/bin/neutron-taas-openvswitch-agent':
-    content => '#!/usr/bin/python
-# PBR Generated from u\'console_scripts\'
-
-import sys
-
-from neutron_lbaas.cmd.lbaasv2_agent import main
-
-
-if __name__ == "__main__":
-    sys.exit(main())
-',
-		owner => 'root',
-		group => 'root',
-		mode  => '0755',
+    source => 'puppet:///modules/openstack/neutron-taas-openvswitch-agent',
+		owner  => 'root',
+		group  => 'root',
+		mode   => '0755',
   }->
   service { 'neutron-taas-openvswitch-agent':
     ensure => running,
     enable => true,
+    tag    => 'neutron-service',
   }
+
+  neutron_taas_agent_config {
+    'taas/driver':              value => $taas_driver;
+    'taas/enabled':             value => $taas_enabled;
+    'taas/vlan_range_start':    value => $taas_vlan_range_start;
+    'taas/vlan_range_end':      value => $taas_vlan_range_end;
+    'DEFAULT/interface_driver': value => $interface_driver;
+  }
+
+  Anchor['neutron::config::begin'] -> Neutron_taas_agent_config<||> ~> Anchor['neutron::config::end']
 
 }
