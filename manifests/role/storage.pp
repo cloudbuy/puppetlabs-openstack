00f0d7cd83801b5d403a41ded5d74b53c18e6426
Damien Churchill <damoxc@gmail.com>
2017-05-11 19:08:21 +0100

    make the storage role configurable as to what is included

--- manifests/role/storage.pp
+++ manifests/role/storage.pp
@@ -1,7 +1,18 @@
-class openstack::role::storage inherits ::openstack::role {
-  #class { '::openstack::profile::firewall': }
-  class { '::openstack::profile::glance::api': }
-  class { '::openstack::profile::cinder::volume': }
+class openstack::role::storage(
+  Boolean $enable_firewall      = false,
+  Boolean $enable_glance_api    = true,
+  Boolean $enable_cinder_volume = true
+) inherits ::openstack::role {
 
-  #class { '::openstack::setup::cirros': }
+  if ($enable_firewall) {
+    class { '::openstack::profile::firewall': }
+  }
+
+  if ($enable_glance_api) {
+    class { '::openstack::profile::glance::api': }
+  }
+
+  if ($enable_cinder_volume) {
+    class { '::openstack::profile::cinder::volume': }
+  }
 }
