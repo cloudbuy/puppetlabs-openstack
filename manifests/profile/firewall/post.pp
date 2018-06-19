132cf78e52912a91577137fa1a809109c43ac6b0
Damien Churchill <damien.churchill@cloudbuy.com>
2018-06-19 11:44:44 +0100

    try applying fix for firewall rules to the firewall/post

--- manifests/profile/firewall/post.pp
+++ manifests/profile/firewall/post.pp
@@ -1,18 +1,18 @@
 # post-firewall rules to reject remaining traffic
 class openstack::profile::firewall::post {
-  firewall { '8999 - Accept all management network traffic':
+  firewall { '88999 - Accept all management network traffic':
     proto  => 'all',
     state  => ['NEW'],
     action => 'accept',
     source => $::openstack::config::network_management,
   } ->
-  firewall { '9100 - Accept all vm network traffic':
+  firewall { '89100 - Accept all vm network traffic':
     proto  => 'all',
     state  => ['NEW'],
     action => 'accept',
     source => $::openstack::config::network_data,
   } ->
-  firewall { '9999 - Reject remaining traffic':
+  firewall { '89999 - Reject remaining traffic':
     proto  => 'all',
     action => 'reject',
     reject => 'icmp-host-prohibited',
